bfc1b47a36d3ce2f4a534eab4c9868922de97423
LAM 1102 <levanlam270920@gmail.com>
2023-11-24 16:54:27 +0700

    lammm

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,20 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}

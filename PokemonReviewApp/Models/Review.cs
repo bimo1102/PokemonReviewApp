b032f6d9c1ece6bc06bcb77856ca0a137e64ee66
LAM 1102 <levanlam270920@gmail.com>
2023-11-24 10:56:16 +0700

    create models

---
+++ PokemonReviewApp/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemonReviewApp.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Reviewer Reviewer { get; set; }
+        public Pokemon pokemon { get; set; }
+    }
+}
